f21f079debc4aef8ca9eba9e92ee5fbca86eb4fc
Reem Madkour <reem.madkour@mail.mcgill.ca>
2018-04-09 09:17:03 -0400

    view for order Item

--- RestoApp/src/ca/mcgill/ecse223/resto/view/OrderItemPage.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/view/OrderItemPage.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse223.resto.view;
 
-import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -10,15 +12,26 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import ca.mcgill.ecse223.resto.application.RestoAppApplication;
+import ca.mcgill.ecse223.resto.controller.InvalidInputException;
+import ca.mcgill.ecse223.resto.controller.RestoAppController;
+import ca.mcgill.ecse223.resto.model.MenuItem;
+import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.model.Seat;
+import ca.mcgill.ecse223.resto.model.Table;
+
 public class OrderItemPage extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtquantity;
 	private int tableNumber = -1;
+	List<Integer>tableNumbers=null;
+	List<Table>selectedTables=null;
 	private JComboBox comboBox;
 	private JComboBox comboBox1;
 	private JComboBox comboBox2;
 	private JButton btnOrder;
+	List<Seat>seats = new ArrayList<Seat>();
 	/**
 	 * 
 	 * Launch the application.
@@ -28,10 +41,12 @@ public class OrderItemPage extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public OrderItemPage(int tableNumber) {
+	public OrderItemPage(List<Table>selectedTables) {
+		//this.tableNumbers=tableNumbers;
+		this.selectedTables=selectedTables;
 		this.tableNumber = tableNumber;
 		initComponents();
-		//refreshData();
+		refreshData();
 		}
 	public void initComponents() {
 		setTitle(" Order Item");
@@ -51,59 +66,141 @@ public class OrderItemPage extends JFrame {
 		
 		txtquantity = new JTextField();
 		txtquantity.setText("quantity");
-		txtquantity.setBounds(20, 15, 100, 30);
+		txtquantity.setBounds(200, 25, 100, 30);
 		contentPane.add(txtquantity);
 		txtquantity.setColumns(10);
 		
-		comboBox2 = new JComboBox();
-		//for (int i = 0; i < restoApp.getCurrentMenuItems().size(); i++) {
-			//comboBox.addItem(restoApp.getCurrentTable(i).getNumber());
-		
-		comboBox2.setBounds(20, 60, 100, 30);
-		contentPane.add(comboBox2);
-
-		JLabel lblChooseOrder = new JLabel("Choose Order");
-		lblChooseOrder.setBounds(20, 35, 100, 30);
-		contentPane.add(lblChooseOrder);
+	
+	
 		
 		
 		
 		comboBox = new JComboBox();
-		//for (int i = 0; i < restoApp.getCurrentMenuItems().size(); i++) {
-			//comboBox.addItem(restoApp.getCurrentTable(i).getNumber());
-		
-		comboBox.setBounds(20, 160, 100, 26);
+		RestoApp restoApp = RestoAppApplication.getRestoApp();
+		for (int i = 0; i < restoApp.getMenu().getMenuItems().size(); i++) {
+			comboBox.addItem( restoApp.getMenu().getMenuItem(i).getName());
+					//.toString());
+		}
+		comboBox.setBounds(20, 35, 100, 26);
 		contentPane.add(comboBox);
 
 		JLabel lblChooseTable = new JLabel("Choose Menu Item");
-		lblChooseTable.setBounds(20, 120, 200, 50);
+		lblChooseTable.setBounds(20, 2, 200, 50);
 		contentPane.add(lblChooseTable);
+		
+		btnOrder = new JButton("Order");
+		btnOrder.setBounds(110, 200, 100, 29);
+		contentPane.add(btnOrder);
+		btnOrder.addActionListener(new java.awt.event.ActionListener(){
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				btnCreateOrderItemActionPerformed(evt); 
+		}
+
+		});
+		contentPane.add(btnOrder);
 		
 		comboBox1 = new JComboBox();
 		//for (int i = 0; i < restoApp.getCurrentMenuItems().size(); i++) {
 			//comboBox.addItem(restoApp.getCurrentTable(i).getNumber());
+		for (int i = 0; i < selectedTables.size(); i++) {
 		
-		comboBox1.setBounds(200, 160, 100, 26);
+			for(int k=0; k<(selectedTables.get(i).getSeats().size());k++){
+				comboBox1.addItem( "Table ".concat(String.valueOf(selectedTables.get(i).getNumber())).concat(
+						", seat:").concat(String.valueOf(k)));
+								//selectedTables.get(i).getSeat(k).toString()) );
+			}	
+		
+		
+		}
+		comboBox1.setBounds(20, 85, 200, 30);
 		contentPane.add(comboBox1);
 
 		JLabel lblChooseTable1 = new JLabel("Choose seats");
-		lblChooseTable1.setBounds(200, 120, 200, 45);
+		lblChooseTable1.setBounds(20, 55, 200, 45);
 		contentPane.add(lblChooseTable1);
 
-		btnOrder = new JButton("Order");
-		btnOrder.setBounds(60, 200, 100, 29);
-		contentPane.add(btnOrder);
-		//btnAddTable.addActionListener(new java.awt.event.ActionListener(){
-			//public void actionPerformed(java.awt.event.ActionEvent evt) {
-				//btnCreateTableActionPerformed(evt); check add table
-		//	}
+		JButton btnNewButton = new JButton("Add Seat");
+	
+		btnNewButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt1) {
+				try {
+					addSeatActionPerformed(evt1);
+				} catch (InvalidInputException e) {
 
-		//});
-		//contentPane.add(btnOrder);
+					e.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setBounds(20, 120, 117, 25);
+		contentPane.add(btnNewButton);}
+	
+		public void addSeatActionPerformed(java.awt.event.ActionEvent evt1) throws InvalidInputException {
+			
+				int l=0;
+			    int found=0;
+			    int q=0;
+			    int w=0;
+				for (int i = 0; i < selectedTables.size(); i++) {
+					
+					for(int k=0; k<(selectedTables.get(i).getSeats().size());k++){
+						if(comboBox1.getSelectedIndex()==l) {
+							found=1;
+							w=k;
+							break;}
+						 l++;}
+					if(found==1) { q=i;
+					              
+						break;}
+					
+				}
+				seats.add(selectedTables.get(q).getCurrentSeat(w));
+			}
+
+		
+		private void refreshData() {// error
+			
+				// populate page with data
+				txtquantity.setText("quantity");
+				
+				RestoApp restoApp = RestoAppApplication.getRestoApp();
+			
+			}
+
+		
+		
+		public void btnCreateOrderItemActionPerformed(java.awt.event.ActionEvent evt) {
+			RestoApp restoApp = RestoAppApplication.getRestoApp();
+			// clear error message
+		
+			// call the controller
+
+		
+
+			MenuItem m= null;
+			for (int i = 0; i < restoApp.getMenu().getMenuItems().size(); i++) {
+				if(comboBox.getSelectedIndex()==i) {m=restoApp.getMenu().getMenuItems().get(i);
+				break;}
+			}
+				int quantity = Integer.parseInt(txtquantity.getText());
+				
+				try {
+					RestoAppController.orderMenuItem(m,quantity,seats);
+				} catch (InvalidInputException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			
+			// update visuals
+		refreshData();	
+		}
+
+		
+		
 	}
 	//}
 	
 	
 	
 
-}
+
